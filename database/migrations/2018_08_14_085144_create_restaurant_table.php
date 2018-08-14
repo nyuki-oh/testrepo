9cf42dbf2bbaf9f97e711b29a23def5879353a1a
EC2 Default User <ec2-user@ip-172-31-24-30.us-east-2.compute.internal>
2018-08-14 09:08:43 +0000

    made migration

---
+++ database/migrations/2018_08_14_085144_create_restaurant_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+use League\Csv\Reader;
+
+class CreateRestaurantTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurant', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('invalid');
+            $table->string('name');
+            $table->string('category_l');
+            $table->string('category_view');
+            $table->string('category_s');
+            $table->string('category');
+            $table->integer('lunch')->nullable();
+            $table->integer('walking')->nullable();
+            $table->float('distance')->nullable();
+            $table->string('url');
+            $table->timestamps();
+        });
+        $reader = Reader::createFromPath('./restaurant.csv');
+        $reader->setHeaderOffset(0);
+        $records = $reader->getRecords();
+        foreach ($records as $r) {
+            print_r($r);
+            $ri = $r;
+            if ($ri['lunch'] == '') {
+                $ri['lunch'] = NULL;
+            }
+            if ($ri['walking'] == '') {
+                $ri['walking'] = NULL;
+            }
+            if ($ri['distance'] == '') {
+                $ri['distance'] = NULL;
+            }
+            DB::table('hoge')->insert($ri);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurant');
+    }
+}
