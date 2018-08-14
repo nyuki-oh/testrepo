5d23af411df5a7dad13ae4c2ea732da46bea0968
EC2 Default User <ec2-user@ip-172-31-24-30.us-east-2.compute.internal>
2018-08-14 10:19:11 +0000

    edit bot reply

--- routes/botman.php
+++ routes/botman.php
@@ -10,9 +10,14 @@ $botman->hears('Hi', function ($bot) {
 $botman->hears('ping', function ($bot) {
     $bot->reply('pong');
 });
-*/
+
 $botman->hears('{mes}', function ($bot, $mes) {
     $bot->reply($mes);
 });
 
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+*/
+$botman->hears('.*(ランチ|らんち|lunch).*', function ($bot) {
+    $lunch = DB::select('select * from restaurant order by RANDOM() limit 3');
+    $bot->reply($lunch[0]->name."\t".$lunch[0]->category_view."\t".$lunch[0]->lunch."\n".$lunch[1]->name."\t".$lunch[1]->category_view."\t".$lunch[1]->lunch."\n".$lunch[2]->name."\t".$lunch[2]->category_view."\t".$lunch[2]->lunch);
+});
