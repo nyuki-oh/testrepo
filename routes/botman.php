b12669e5fbbe9dfa3a5afcc6949f1a2324ed5068
fumiya.niikura <gitgitgiton@gmail.com>
2018-08-15 02:12:01 +0000

    conv

--- routes/botman.php
+++ routes/botman.php
@@ -14,9 +14,9 @@ $botman->hears('ping', function ($bot) {
 $botman->hears('{mes}', function ($bot, $mes) {
     $bot->reply($mes);
 });
-
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
 */
+$botman->hears('Start conversation', BotManController::class.'@startConversation');
+
 $botman->hears('.*(ランチ|らんち|lunch).*', function ($bot) {
     $lunch = DB::select('select * from restaurant order by RANDOM() limit 3');
     $bot->reply($lunch[0]->name."\t".$lunch[0]->category_view."\t".$lunch[0]->lunch."\n".$lunch[1]->name."\t".$lunch[1]->category_view."\t".$lunch[1]->lunch."\n".$lunch[2]->name."\t".$lunch[2]->category_view."\t".$lunch[2]->lunch);
