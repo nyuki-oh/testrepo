7a7e603f329d443f23121855838e5a24a95601e0
fumiya.niikura <gitgitgiton@gmail.com>
2018-08-15 02:35:53 +0000

    change wakeup word_

--- routes/botman.php
+++ routes/botman.php
@@ -15,7 +15,7 @@ $botman->hears('{mes}', function ($bot, $mes) {
     $bot->reply($mes);
 });
 */
-$botman->hears('Start conversation', BotManController::class.'@startConversation');
+$botman->hears('yo', BotManController::class.'@startConversation');
 
 $botman->hears('.*(ランチ|らんち|lunch).*', function ($bot) {
     $lunch = DB::select('select * from restaurant order by RANDOM() limit 3');
