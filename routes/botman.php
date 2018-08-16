ff5311eb0e510175558c4001485acf23793db5f2
fumiya.niikura <gitgitgiton@gmail.com>
2018-08-16 04:04:46 +0000

    single query

--- routes/botman.php
+++ routes/botman.php
@@ -35,7 +35,7 @@ $botman->hears('(ランチ|らんち|lunch)( |　)+{msg}', function ($bot, $msg)
     $bot->reply("> <".$lunch[0]->url."|".$lunch[0]->name.">\t".$lunch[0]->category_view."\t".$lunch[0]->lunch."円   徒歩".$lunch[0]->walking."分\n><".$lunch[1]->url."|".$lunch[1]->name.">\t".$lunch[1]->category_view."\t".$lunch[1]->lunch."円   徒歩".$lunch[1]->walking."分\n><".$lunch[2]->url."|".$lunch[2]->name.">\t".$lunch[2]->category_view."\t".$lunch[2]->lunch."円   徒歩".$lunch[2]->walking."分");
     });
 
-// $botman->hears('(ランチ|らんち|lunch)', function ($bot) {
-//     $lunch = DB::select('select * from restaurant order by RANDOM() limit 3');
-//     $bot->reply("> <".$lunch[0]->url."|".$lunch[0]->name.">\t".$lunch[0]->category_view."\t".$lunch[0]->lunch."円   徒歩".$lunch[0]->walking."分\n><".$lunch[1]->url."|".$lunch[1]->name.">\t".$lunch[1]->category_view."\t".$lunch[1]->lunch."円   徒歩".$lunch[1]->walking."分\n><".$lunch[2]->url."|".$lunch[2]->name.">\t".$lunch[2]->category_view."\t".$lunch[2]->lunch."円   徒歩".$lunch[2]->walking."分");
-// });
+$botman->hears('(ランチ|らんち|lunch)', function ($bot) {
+    $lunch = DB::select('select * from restaurant order by RANDOM() limit 3');
+    $bot->reply("> <".$lunch[0]->url."|".$lunch[0]->name.">\t".$lunch[0]->category_view."\t".$lunch[0]->lunch."円   徒歩".$lunch[0]->walking."分\n><".$lunch[1]->url."|".$lunch[1]->name.">\t".$lunch[1]->category_view."\t".$lunch[1]->lunch."円   徒歩".$lunch[1]->walking."分\n><".$lunch[2]->url."|".$lunch[2]->name.">\t".$lunch[2]->category_view."\t".$lunch[2]->lunch."円   徒歩".$lunch[2]->walking."分");
+});
